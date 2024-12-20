7b02b019a4fce4d73def2e90f4c8d0a165ef720a
manoelerickgp <manoel.computacao@gmail.com>
2024-12-19 21:42:34 -0300

    testando start da aplicacao e request GET

--- src/main/java/com/tech/students/StudentsApplication.java
+++ src/main/java/com/tech/students/StudentsApplication.java
@@ -2,12 +2,20 @@ package com.tech.students;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsApplication.class, args);
+	}
+
+	@GetMapping(value ="/")
+	public String hello() {
+		return "Hello world!";
 	}
 
 }
