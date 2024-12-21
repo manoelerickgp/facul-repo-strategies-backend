3ee2248c5fa6d8817fe6ad11b42fbd01b2a0a4d4
manoelerickgp <manoel.computacao@gmail.com>
2024-12-20 22:52:42 -0300

    add: adicionando entidade e service

---
+++ src/main/java/com/tech/students/domain/Student.java
@@ -0,0 +1,77 @@
+package com.tech.students.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Student {
+
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String email, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
---
+++ src/main/java/com/tech/students/service/StudentService.java
@@ -0,0 +1,57 @@
+package com.tech.students.service;
+
+import com.tech.students.domain.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class StudentService {
+
+    private static Map<Long, Student> studentList = new HashMap<>();
+
+    private ResponseEntity<Student> getStudentById(Long id) {
+        Student student = studentList.get(id);
+        if (student == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        return ResponseEntity.status(HttpStatus.OK).body(student);
+    }
+
+    private List<Student> getAllStudents() {
+        return new ArrayList<>(studentList.values());
+    }
+
+    private ResponseEntity<Student> save(Long id, Student studentReq) {
+        Student student = studentList.get(studentReq.getId());
+        if (student !=  null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        if (studentList.containsKey(id)) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+        }
+        studentList.put(id, studentReq);
+        return ResponseEntity.status(HttpStatus.OK).body(studentReq);
+    }
+
+    private ResponseEntity<Student> update(Long id, Student studentReq) {
+        boolean existsStudent = studentList.containsKey(id);
+        if (existsStudent) {
+            return ResponseEntity.status(HttpStatus.OK).body(studentReq);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(studentReq);
+    }
+
+    private ResponseEntity<String> delete(Long id) {
+        boolean existsStudent = studentList.containsKey(id);
+        if (!existsStudent) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found");
+        }
+        studentList.remove(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Student Deleted  successfully");
+    }
+}
