41bf64094cce9c7ed504b504ec76e77474af777a
manoelerickgp <manoel.computacao@gmail.com>
2024-12-24 09:48:37 -0300

    add: metodo para salvar o arquivo

--- src/main/java/com/tech/students/service/FileService.java
+++ src/main/java/com/tech/students/service/FileService.java
@@ -5,11 +5,14 @@ import com.tech.students.exception.UploadFileException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileService {
@@ -24,6 +27,32 @@ public class FileService {
             Files.createDirectories(this.fileStorageLocation);
         } catch (IOException e) {
             throw new UploadFileException("Algo deu errado ao tentar criar a pasta", e);
+        }
+    }
+
+    /**
+     *  Este método salva um arquivo
+     * @param file armazena o arquivo
+     * A variável 'fileName' recebe apenas o nome do arquivo
+     * O 'IF' verifica se o arquivo é válido
+     * A variável 'targetLocation' verifica se o arquivo já existe ou não para dar sequência oa resto do fluxo
+     * O método 'copy' da classe File usa o inputStream para pegar o tod0 o conteudo do arquivo, depois, usa o 'targetLocation'
+     * para concatenar o diretório com o nome do arquivo e, depois disso, é usado REPLACE_EXISTING para caso já exista um arquivo
+     * com o mesmo nome do que está sendo salvo, irá substituir o conteudo do arquivo já existente
+     * @return retorna o nome do arquivo
+     */
+    public String saveFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            if (fileName.contains("..")) {
+                throw new UploadFileException("Invalid file");
+            }
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return fileName;
+        } catch (IOException e) {
+            throw new UploadFileException("Erro ao tentar salvar o arquivo");
         }
     }
 
